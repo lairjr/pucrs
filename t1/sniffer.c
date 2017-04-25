28f3c7fccfbf8e6015f2cf22045d40678ed5c693
lairjr <lairjr@outlook.com>
2017-04-25 17:17:03 -0300

    Add function to count protocols

--- t1/sniffer.c
+++ t1/sniffer.c
@@ -18,8 +18,12 @@
 
 #define BUFFSIZE 1518
 
-#define IPV6 0x86
-#define IPV4 0x08
+#define HEX01 0x01
+#define HEX06 0x06
+#define HEX08 0x08
+#define HEX11 0x11
+#define HEX86 0x86
+#define HEX3a 0x3a
 
 // Atencao!! Confira no /usr/include do seu sisop o nome correto
 // das estruturas de dados dos protocolos.
@@ -32,10 +36,23 @@ int ipv6_counter, ipv4_counter, arp_counter, icmp_counter, icmpv6_counter, udp_c
 
 void indentify_package()
 {
-		if (buff1[12] == IPV6) {
+		if (buff1[13] == HEX06) {
+				arp_counter++;
+		}
+		if (buff1[12] == HEX86) {
 				ipv6_counter++;
-		} else if (buff1[12] == IPV4) {
+				if (buff1[20] == HEX3a) {
+						icmpv6_counter++;
+				}
+		} else if (buff1[12] == HEX08) {
 				ipv4_counter++;
+				if (buff1[23] == HEX11) {
+						udp_counter++;
+				} else if (buff1[23] == HEX06) {
+						tcp_counter++;
+				} else if (buff1[23] == HEX01) {
+						icmp_counter++;
+				}
 		}
 }
 
@@ -49,14 +66,14 @@ void print_package()
 		printf("MAC Destino: %x:%x:%x:%x:%x:%x \n", buff1[0],buff1[1],buff1[2],buff1[3],buff1[4],buff1[5]);
 		printf("MAC Origem:  %x:%x:%x:%x:%x:%x \n", buff1[6],buff1[7],buff1[8],buff1[9],buff1[10],buff1[11]);
 		printf("IP version %x:%x\n", buff1[12], buff1[13]);
-		printf("Protocolo %x\n", buff1[20]);
+		printf("Protocolo %x\n", buff1[22]);
 }
 
 void print_summary()
 {
 		printf("\nTotal packages\n");
 		printf("IPv4: %d | IPv6: %d | ARP: %d | ICMP: %d | ICMPv6: %d | UDP: %d | TCP: %d\n",
-		       ipv6_counter, ipv4_counter, arp_counter, icmp_counter, icmpv6_counter, udp_counter, tcp_counter);
+		       ipv4_counter, ipv6_counter, arp_counter, icmp_counter, icmpv6_counter, udp_counter, tcp_counter);
 }
 
 int main(int argc,char *argv[])
