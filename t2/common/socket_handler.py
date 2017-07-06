2abb325ddc6efc24cbbe8dcab8154bbc0b22e831
lairjr <lairjr@outlook.com>
2017-07-06 18:53:32 -0300

    Add initial raw implementation

--- t2/common/socket_handler.py
+++ t2/common/socket_handler.py
@@ -10,7 +10,7 @@ class SocketHandler:
             raise ValueError("An instantiation already exists!")
         self.destination_port = destination_port
         self.source_port = source_port
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 
     @classmethod
     def get_instance(cls, destination_port, source_port):
@@ -28,10 +28,25 @@ class SocketHandler:
     def sendto(self, message, destination):
         print("Sent to: " + str(destination))
 
+        # eth_heeader =  struct.pack('!HHHH', self.source_port, self.destination_port, length, checksum)
+
+        ip_ihl_ver = 5
+        ip_tos = 4
+        ip_tot_len = 0
+        ip_id = 0
+        ip_frag_off = 0
+        ip_ttl = 255
+        ip_proto = socket.IPPROTO_UDP
+        ip_check = 0
+        ip_saddr = self.s.getsockname()[0]
+        ip_daddr = destination[0]
+        ip_header = struct.pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+
         length = 8+len(message);
         checksum = 0
         udp_header = struct.pack('!HHHH', self.source_port, self.destination_port, length, checksum)
-        self.s.sendto(udp_header + message, destination)
+
+        self.s.sendto(ip_header + udp_header + message, destination)
 
     def receivefrom(self):
         packet, received_address = self.s.recvfrom(1024)
